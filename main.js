74f9f80c58e171c4adf2634cb5213f90b96cd257
Victoria D. <tovictoriadao@gmail.com>
2026-02-05 13:19:18 -0500

    Activity 2 complete

--- main.js
+++ main.js
@@ -30,3 +30,47 @@ g.attr("fill", "steelblue")
 
 g.attr('transform', "translate(50,50)")
 
+// activity 2
+function getData() { //generate an array of random numbers
+    let data = [];
+    let itemSize = 0;
+
+
+    for (let i = 0; i < 10; i++) {
+        itemSize = Math.ceil(Math.random() * 5) * 10;
+        data.push(itemSize);
+    }
+    return data;
+}
+
+function update(data) {
+    d3.select('#vis2')
+        .selectAll('circle')
+        .data(data)
+        .join(
+            function (enter) {
+                return enter.append('circle')
+                    .style('opacity', 0.25);
+            },
+            function (update) {
+                return update.style('opacity', 1);
+            }
+        )
+        .attr('cx', function (d, i) {
+            return i * 100;
+        })
+        .attr('cy', 50)
+        .attr('r', function (d) {
+            return 0.5 * d;
+        })
+        .style('fill', 'orange');
+}
+function updateAll() {
+    let myData = getData(); //get new data from random generator
+    update(myData); //update the visualization
+}
+updateAll(); //get new data and visualize (called one time only)
+
+//select button on the page, add a click event to it.  
+d3.select("button")
+    .on("click", updateAll);
